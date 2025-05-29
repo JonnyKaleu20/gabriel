ed279281ae610f56b8ad9a07e3b36d2dbf70d132
JonnyKaleu20 <jonnibomgarte@gmail.com>
2025-05-28 21:04:22 -0300

    Botando pa roda o chat

--- Js/batepapo.js
+++ Js/batepapo.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyAUeTQIepB0g7eTE2P6j5bBcmrReYvpy6Q",
+  authDomain: "gabriel-54695.firebaseapp.com",
+  projectId: "gabriel-54695",
+  storageBucket: "gabriel-54695.firebasestorage.app",
+  messagingSenderId: "226444745077",
+  appId: "1:226444745077:web:96d3ded13a94266712db3c",
+  // ...
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Lendo uma coleção pública
+const querySnapshot = await getDocs(collection(db, "minhaColecao"));
+querySnapshot.forEach((doc) => {
+  console.log(doc.id, " => ", doc.data());
+});
+
 const chatBox = document.getElementById("chat-box");
 const form = document.getElementById("chat-form");
 const userInput = document.getElementById("user");
